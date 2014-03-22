485b111410b5bd0dba92ecf279a2d0b0dd047f02
pierre.taillard <pierre_taillard@yahoo.fr>
2014-03-22 13:37:24 +0100

    Accueil new design - cleaner

--- scripts/directives/infosDirective.js
+++ scripts/directives/infosDirective.js
@@ -8,13 +8,13 @@ AgileGrenobleApp.directive('agInfos', ['$window', function($window) {
 			controller: function ($scope) {
 				$scope.site = function(annee) {
 					if(annee <= 2009) {
-						$window.location.href = 'http://at' + annee + '.agiletour.org/';
+						$window.open('http://at' + annee + '.agiletour.org/');
 					}
 					else if(annee <= 2011) {
-						$window.location.href = 'http://agile-grenoble.org/' + annee + '/start';
+						$window.open('http://agile-grenoble.org/' + annee + '/start');
 					}
 					else {
-						$window.location.href = 'http://' + annee + '.agile-grenoble.org';
+						$window.open('http://' + annee + '.agile-grenoble.org');
 					}
 				};
 			},
